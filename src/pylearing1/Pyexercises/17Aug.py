4d8ba28cfceba5efb05b81715bde35210ce014b0
Suryanadh91 <suryanadh91@gmail.com>
2024-08-19 12:10:12 +0530

    updating exercise

---
+++ src/pylearing1/Pyexercises/17Aug.py
@@ -0,0 +1,29 @@
+import math
+
+# Task 5 take 2 no.s print whether a is > or < or == to b
+a = int(input("num1"))
+b = int(input("num2))
+g = a > b
+l = a < b
+e = a == b
+print("a greater than b", g)
+print("a less than b", l)
+print("a is equal to b", e)
+print('#######################################')
+# Task 6 square and cube root of a number
+number = float(input("number" ))
+sq = number**2
+sqroot = math.sqrt (number)
+cube = math.pow(number,3)
+print(sq)
+print(sqroot)
+print(cube)
+print('#######################################')
+
+name = input("what is your name")
+age = input("what is your age")
+roll_num = input("what is your num")
+print(name)
+print(age)
+print(roll_num)
+print('####################################')
