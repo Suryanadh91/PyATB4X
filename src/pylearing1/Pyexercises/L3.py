053b829cf5802e87d74845355b2f04940350a652
Suryanadh91 <suryanadh91@gmail.com>
2024-08-21 11:45:01 +0530

    updating exercise

--- src/pylearing1/Pyexercises/L3.py
+++ src/pylearing1/Pyexercises/L3.py
@@ -8,7 +8,7 @@ print(max_num)
 # with if else
 if num1 > num2 and num1 > num3:
     print("Max  is",num1)
-elif num1 > num2 and num1 > num3:
+elif num2 > num1 and num2 > num3:
     print("Max  is",num2)
 else:
     print("Max  is",num3)
