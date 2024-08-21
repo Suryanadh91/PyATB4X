8d31041277ea94146635d9421815f7c119676662
Suryanadh91 <suryanadh91@gmail.com>
2024-08-21 12:28:30 +0530

    updating exercise

--- src/pylearing1/Pyexercises/20Aug.py
+++ src/pylearing1/Pyexercises/20Aug.py
@@ -1,36 +1,4 @@
-# find the highest number
-num1 = int(input("Enter num 1"))
-num2 = int(input("Enter num 2"))
-num3 = int(input("Enter num 3"))
-
-# 1 type of logic using max function
-max_num = max(num1,num2,num3)
-print(max_num)
-
-# with if else
-if num1 > num2 and num1 > num3:
-    print("Max  is",num1)
-elif num2 > num1 and num2 > num3:
-    print("Max  is",num2)
-else:
-    print("Max  is",num3)
-
-# program for grading the score
-score = int(input("Give your score\n"))
-if score >= 90 and score <= 100:
-    print("A grade")
-elif score >= 80 and score <= 89:
-    print("B grade")
-elif score >= 70 and score <= 79:
-    print("C grade")
-elif score >=60 and score <=69:
-    print("D grade")
-elif score >=0 and score <=59:
-    print("F grade")
-else:
-    print("Invalid output")
-
-# Task 8 find the leap year
+# Task 7 find the leap year
 
 year = int(input("Enter the year\n"))
 if year % 4 == 0 and year % 100!= 0:
@@ -40,7 +8,7 @@ elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
 else:
     print("not leap year")
 
-# Task 9 find the type of triangle
+# Task 8 find the type of triangle
 side_1 = int(input("enter side1 dimensions\n"))
 side_2 = int(input("enter side2 dimensions\n"))
 side_3 = int(input("enter side3 dimensions\n"))
@@ -57,3 +25,14 @@ elif side_1 != side_2 != side_3:
     print("scalen_triangle")
 else:
     print("Invalid data")
+
+# Task 9
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+         print('Buzz')
+    else:
+        print(number)
--- src/pylearing1/Pyexercises/L4.py
+++ src/pylearing1/Pyexercises/L4.py
@@ -1,25 +1,31 @@
-# find the type of triangle
+# find the highest number
+num1 = int(input("Enter num 1"))
+num2 = int(input("Enter num 2"))
+num3 = int(input("Enter num 3"))
 
-side_1 = int(input("enter side1 dimensions\n"))
-side_2 = int(input("enter side2 dimensions\n"))
-side_3 = int(input("enter side3 dimensions\n"))
-if side_1 == side_2 == side_3:
-    print("equal triangle")
-elif (side_1 == side_2 and  side_1 != side_3
-      or ((((side_1 == side_3 and side_1 != side_2
-        or side_2 == side_1 and side_2 != side_3)
-        or side_2 == side_3 and side_2 != side_1)
-        or side_3 == side_1 and side_3 != side_2)
-        or side_3 == side_2 and side_3 != side_1)):
-    print("iso_traiangle")
-elif side_1 != side_2 != side_3:
-    print("scalen_triangle")
+# 1 type of logic using max function
+max_num = max(num1,num2,num3)
+print(max_num)
+
+# with if else
+if num1 > num2 and num1 > num3:
+    print("Max  is",num1)
+elif num2 > num1 and num2 > num3:
+    print("Max  is",num2)
 else:
-    print("Invalid data")
+    print("Max  is",num3)
 
-# Factorial
-n = 5
-a = 1
-for i in range (1,n+1):
-    a = a * i
-print(a)
+# program for grading the score
+score = int(input("Give your score\n"))
+if score >= 90 and score <= 100:
+    print("A grade")
+elif score >= 80 and score <= 89:
+    print("B grade")
+elif score >= 70 and score <= 79:
+    print("C grade")
+elif score >=60 and score <=69:
+    print("D grade")
+elif score >=0 and score <=59:
+    print("F grade")
+else:
+    print("Invalid output")
