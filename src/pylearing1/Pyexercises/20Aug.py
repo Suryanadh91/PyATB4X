111f1773c14514c0217d919b78f1ec8cce8c7a5f
Suryanadh91 <suryanadh91@gmail.com>
2024-08-21 11:57:33 +0530

    updating exercise

--- src/pylearing1/Pyexercises/20Aug.py
+++ src/pylearing1/Pyexercises/20Aug.py
@@ -30,7 +30,7 @@ elif score >=0 and score <=59:
 else:
     print("Invalid output")
 
-# find the leap year
+# Task 8 find the leap year
 
 year = int(input("Enter the year\n"))
 if year % 4 == 0 and year % 100!= 0:
@@ -39,3 +39,21 @@ elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
     print("leap year")
 else:
     print("not leap year")
+
+# Task 9 find the type of triangle
+side_1 = int(input("enter side1 dimensions\n"))
+side_2 = int(input("enter side2 dimensions\n"))
+side_3 = int(input("enter side3 dimensions\n"))
+if side_1 == side_2 == side_3:
+    print("equal triangle")
+elif (side_1 == side_2 and  side_1 != side_3
+      or ((((side_1 == side_3 and side_1 != side_2
+        or side_2 == side_1 and side_2 != side_3)
+        or side_2 == side_3 and side_2 != side_1)
+        or side_3 == side_1 and side_3 != side_2)
+        or side_3 == side_2 and side_3 != side_1)):
+    print("iso_traiangle")
+elif side_1 != side_2 != side_3:
+    print("scalen_triangle")
+else:
+    print("Invalid data")
